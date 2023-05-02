72c9e212ac020d238364b1e95a24fffd8c181a11
지호 <kjh070612123234@gmail.com>
2023-05-02 07:21:22 +0000

    2023프로그래밍 수업

---
+++ src/62.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a = 3;
+	printf("%d\n", &a);
+	printf("%d\n", *&a);
+	return 0;
+}
---
+++ src/l4.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int num = 1640;
+	printf("%d", num >> 3);
+	return 0;
+}
